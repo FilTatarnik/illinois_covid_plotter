c996a51ee8dd887dc765a934550ddf4313e086c7
Filip Tatarnik <ftatarnik@gmail.com>
2021-05-06 17:49:33 -0500

    Updated comments in code for readablility

--- pandas_covid.py
+++ pandas_covid.py
@@ -1,13 +1,13 @@
+# Importing dependencies
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Reading JSON Object of Total_Tested
 illinois_covid_numbers = pd.read_json(
     path_or_buf='https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetIllinoisCases')
 
-
-#illi_nums = pd.DataFrame(illinois_covid_numbers)
-
-#pd.plotting.andrews_curves(illi_nums, 'confirmed_cases')
+# Plotting Data from 5 different sources
 illinois_covid_numbers.plot(figsize=(6, 6), subplots=True)
+
+# Showing plotted data
 plt.show()
